3f760b362e68458fe37706f7272ad6193cd0a9d6
Pampoozle <momomorissette@hotmail.com>
2024-08-30 16:03:16 -0400

    created a FPS limiter using pygame.time.Clock

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,8 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick())/1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
