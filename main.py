498a4663997fbc69e02940f0e6c0dce8b9b15556
Pampoozle <momomorissette@hotmail.com>
2024-08-30 17:11:46 -0400

    created the player's triangle shape and incorporated it the game

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import pygame
 
 from constants import *
+from player import *
 
 
 def main():
+    #VARIABLE CONSTRUCTION SITE
     pygame.init()
     clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #GAME LOOP
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        dt = (clock.tick(60)) / 1000
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = (clock.tick())/1000
+        
+        
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
