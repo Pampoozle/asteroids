1b98468db5b072c421cf32d8930064306f25ab59
Pampoozle <momomorissette@hotmail.com>
2024-09-03 10:33:52 -0400

    Created updatable and drawable groups and updated the code to call the groups

--- main.py
+++ main.py
@@ -5,24 +5,40 @@ from player import *
 
 
 def main():
+    
     #VARIABLE CONSTRUCTION SITE
     pygame.init()
     clock = pygame.time.Clock()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    #GROUPS
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     #GAME LOOP
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+            
         dt = (clock.tick(60)) / 1000
-        player.draw(screen)
-        player.update(dt)
+        
+        for obj in updatable:
+            obj.update(dt)
+
+        pygame.Surface.fill(screen, (0,0,0))
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
         
         
     print("Starting asteroids!")
