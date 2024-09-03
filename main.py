c84db237dc06a0d720162d86679c720f3089fcbf
Pampoozle <momomorissette@hotmail.com>
2024-09-03 12:51:27 -0400

    added a shoot and a collision function for the player

--- main.py
+++ main.py
@@ -1,58 +1,57 @@
+import sys
 import pygame
-
-from asteroid import Asteroid
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
 from asteroidfield import AsteroidField
-
+from shot import Shot
 
 
 def main():
-    
-    #VARIABLE CONSTRUCTION SITE
     pygame.init()
-    clock = pygame.time.Clock()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-
-    #GROUPS
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    Asteroid.containers = (asteroids,updatable,drawable)
-    AsteroidField.containers = (updatable)
+    shots = pygame.sprite.Group()
 
-    #INITIALISATION
-    player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
-    #GAME LOOP
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        dt = (clock.tick(60)) / 1000
-        
+
         for obj in updatable:
             obj.update(dt)
 
-        pygame.Surface.fill(screen, (0,0,0))
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
 
         for obj in drawable:
             obj.draw(screen)
 
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-        
-        
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
     main()
+ 
